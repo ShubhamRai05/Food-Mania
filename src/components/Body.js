aa6483adb76364f20684f253a20a45ba4b65fb7e
Shubham Rai <shubham56237@gmail.com>
2023-08-19 23:28:24 +0530

    adding search part

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,19 @@
-import {restaurantCardData} from "./config";
+import { restaurantCardData } from "./config";
 import RestaurantCard from "./RestaurantCard";
 
 
 const BodyComponent = function () {
+    // let searchText = "hello guys " this is how we do in javascript
     return (
-        <div className="restaurant-list">
-            {restaurantCardData.map((restro) => {
-                return <RestaurantCard {...restro.info} key={restro.info.id} />
-            })}
-        </div>
+        <>
+            <input type="text" className="search-input" placeholder="Search here" value="" onChange="" />
+            <button>Search</button>
+            <div className="restaurant-list">
+                {restaurantCardData.map((restro) => {
+                    return <RestaurantCard {...restro.info} key={restro.info.id} />
+                })}
+            </div></>
+
     )
 }
 
