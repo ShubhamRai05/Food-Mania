9871163f9772f34c054a8b0a5798f44efa834ae1
Shubham Rai <shubham56237@gmail.com>
2023-08-21 23:24:55 +0530

    updating search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,38 @@
 import { restaurantCardData } from "./config";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 
 const BodyComponent = function () {
     // let searchText = "hello guys " this is how we do in javascript
+    const [searchText, setSearchText] = useState("")
+    let [searchButton, setSearchButton] = useState("true")
     return (
         <>
-            <input type="text" className="search-input" placeholder="Search here" value="" onChange="" />
-            <button>Search</button>
+            <input type="text" className="search-input" placeholder="Search here" value={searchText} onChange={(e) => {
+                setSearchText(e.target.value)
+            }} />
+
+
+            <button onClick={() => {
+            // if(searchButton === "true"){
+            //     setSearchButton("false")
+            // }
+            // else
+            // {
+            //     setSearchButton("true")
+            // }
+
+            // above using ternary operator
+
+            searchButton === "true" ? setSearchButton("false") : setSearchButton("true")
+            }} value={searchButton}>Search</button>
+
+            {/* two way data binding because it is reading and writing the value */}
+            <h1>{searchButton}</h1>
+            <h1>{searchText}</h1>
+
+
             <div className="restaurant-list">
                 {restaurantCardData.map((restro) => {
                     return <RestaurantCard {...restro.info} key={restro.info.id} />
