d4e67e1661a41d108a2a2c515373d4d2df63bbb9
Shubham Rai <shubham56237@gmail.com>
2023-08-22 23:09:15 +0530

    working with search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,41 +2,41 @@ import { restaurantCardData } from "./config";
 import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
 
-
+function filterData(restaurant, searchText) {
+    let data = restaurant.filter((restaurant) =>restaurant.info.name.includes(searchText))
+    return data;
+}
 const BodyComponent = function () {
     // let searchText = "hello guys " this is how we do in javascript
     const [searchText, setSearchText] = useState("")
-    let [searchButton, setSearchButton] = useState("true")
+    // let [searchButton, setSearchButton] = useState("")
+    let [restaurant, setRestaurant] = useState(restaurantCardData)
     return (
         <>
             <input type="text" className="search-input" placeholder="Search here" value={searchText} onChange={(e) => {
                 setSearchText(e.target.value)
             }} />
 
-
+                
             <button onClick={() => {
-                // if(searchButton === "true"){
-                //     setSearchButton("false")
-                // }
-                // else
-                // {
-                //     setSearchButton("true")
-                // }
+                
+                console.log("he")
+                let data = filterData(restaurant, searchText)
+                if(data){
+                    setRestaurant(data)
+                }
+                else
+                {
+                    setRestaurant(restaurant)
+                }
+ 
+            }} >Search</button>
 
-                // above using ternary operator
-                console.log(setSearchButton)
-
-                searchButton === "true" ? setSearchButton("false") : setSearchButton("true")
-            }} value={searchButton}>Search</button>
-
-            {/* two way data binding because it is reading and writing the value */}
-            <h1>{searchButton}</h1>
-            <h1>{searchText}</h1>
-            <h1>{setSearchButton}</h1>
+        
 
 
             <div className="restaurant-list">
-                {restaurantCardData.map((restro) => {
+                {restaurant.map((restro) => {
                     return <RestaurantCard {...restro.info} key={restro.info.id} />
                 })}
             </div></>
